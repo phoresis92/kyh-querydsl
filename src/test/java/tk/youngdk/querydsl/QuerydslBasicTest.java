abc04bb1b01fc26ca20dbab937bd9d2b3ae7edd3
kyd <husheart@naver.com>
2020-10-23 00:46:46 +0900

    4 - 시작 JPQL vs Querydsl
    기본 문법

--- src/test/java/tk/youngdk/querydsl/QuerydslBasicTest.java
+++ src/test/java/tk/youngdk/querydsl/QuerydslBasicTest.java
@@ -1,5 +1,69 @@
 package tk.youngdk.querydsl;
 
-@
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import tk.youngdk.querydsl.entity.Member;
+import tk.youngdk.querydsl.entity.QMember;
+import tk.youngdk.querydsl.entity.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
 public class QuerydslBasicTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory query;
+
+    @BeforeEach
+    public void beforeEach(){
+        query = new JPAQueryFactory(em);
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 20, teamA);
+        Member member3 = new Member("member3", 30, teamB);
+        Member member4 = new Member("member4", 40, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+    }
+
+    @Test
+    public void startJpql() throws Exception {
+        // member1을 찾아라
+        Member findByJpql = em.createQuery("select m from Member m where m.username = :username", Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findByJpql.getUsername()).isEqualTo("member1");
+    }
+
+    @Test
+    public void startQuerydsl () throws Exception {
+//        QMember m = new QMember("m");
+        QMember m = QMember.member;
+
+        Member findByQuerydsl = query.select(m)
+                .from(m)
+                .where(m.username.eq("member1"))
+                .fetchOne();
+
+        assertThat(findByQuerydsl.getUsername()).isEqualTo("member1");
+    }
 }
